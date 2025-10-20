d8d58bf0a29364c483b4e5110dea00ee017599c4
VilMe <vilma.medrano00@gmail.com>
2025-10-19 23:15:32 -0500

    continue defining shorten_link function

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ BASE_URL: Final[str] = 'https://cutt.ly/api/api.ph'
 
 def shorten_link(full_link: str):
     payload: dict = {'key': API_KEY, 'short': full_link}
-    request = requests.get(BASE_URL)
+    request = requests.get(BASE_URL, params=payload)
+    data: dict = request.json()
