15e70aaebfcb1cf33598302a821b706972cad628
VilMe <vilma.medrano00@gmail.com>
2025-10-19 22:53:15 -0500

    start define function shorten link

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ import os
 API_KEY: Final[str] = os.environ["CUTTLY_API_KEY"]
 BASE_URL: Final[str] = 'https://cutt.ly/api/api.ph'
 
+
+def shorten_link(full_link: str):
+    payload: dict = {'key': API_KEY, 'short': full_link}
+    request = requests.get(BASE_URL)
